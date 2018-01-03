4c7a80d7b5983bb2bad65c754da3b6a801318615
yulricsequeira <yulrics@gmail.com>
2018-01-03 11:53:57 +0400

    [Feature] Added addin files

---
+++ addin.R
@@ -0,0 +1,15 @@
+library('rstudioapi')
+library('glue')
+source(file.path(getwd(), 'project-builder.R'))
+
+rProjectBuilderAddin <- function() {
+  webSpecCsvFilePath <- rstudioapi::getActiveDocumentContext()$path
+  
+  outDirPath <- file.path(getwd(), 'generated-project')
+  if (dir.exists(outDirPath) == FALSE) {
+    dir.create(outDirPath)
+  }
+  buildProjectFiles(webSpecCsvFilePath, outDirPath)
+  
+  print(glue::glue('Done. The generated project should be in the {outDirPath} folder'))
+}
