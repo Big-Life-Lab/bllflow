e1e6afd98c279c0912e7023dd0ffaaefc7e13280
Yulric <yulrics@OHRIMAC990212.local>
2018-01-08 16:01:50 -0500

    [Feature] Add line breaks between each dummy variable code

--- R/dummy-variable.R
+++ R/dummy-variable.R
@@ -4,7 +4,12 @@ getCodeForDummyVariable <- function(dummyVariableName, numberOfCategories) {
   code <- ''
   
   for (i in 1:numberOfCategories) {
-    code <- paste(code, glue::glue('{dummyVariableName}_{i-1} <- ifelse({dummyVariableName} == {i}, 1, 0)'), sep="")
+    generatedDummyVariableCode <- glue::glue('{dummyVariableName}_{i-1} <- ifelse({dummyVariableName} == {i}, 1, 0)')
+    if(i == 1) {
+      code <- glue::glue('{code}{generatedDummyVariableCode}')
+    } else {
+      code <- glue::glue('{code}\n{generatedDummyVariableCode}')
+    }
   }
   
   return(code)
