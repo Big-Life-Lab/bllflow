48f8383822d853dcb510fa8108851cd925e70a57
Yulric Sequeira <yulrics@gmail.com>
2018-10-16 11:57:53 -0400

    [Refactor] Added comments

--- R/centered-variable.R
+++ R/centered-variable.R
@@ -1,5 +1,22 @@
 library(glue)
 
+#' Returns R code to center a variable
+#'
+#' @param uncenteredVariableName The name of the variable to center
+#' @param centeredVariableName The name of the variable after it has been centered
+#'
+#' @return string
+#' @export
+#'
+#' @examples
 getCodeForCenteredVariable <- function(uncenteredVariableName, centeredVariableName) {
+  # The final code looks like:
+  # BB6_1_c <- BB6_1 - means[means$Name=="BB6_1_c", ]$Means
+  # where uncenteredVariableName = BB6_1_c and centeredVariableName = BB6_1
+  # The code makes the folloowing assumptions:
+  # 1. There is a variable called means
+  # 2. means is a dataframe with a Name and a Means column
+  # 3. Name matches with the name of the centeredVariableName var and Means
+  #    has the mean for it
   return(glue::glue('{centeredVariableName} <- {uncenteredVariableName} - means[means$Name=="{centeredVariableName}", ]$Means'))
 }
--- R/project-builder.R
+++ R/project-builder.R
@@ -4,44 +4,84 @@ source(file.path(getwd(), 'R/interactions.R'))
 
 VariableNameColumName <- 'Variable_Name'
 
+#' Builds several R files from the web spec csv file located at the path 
+#' specified by the webSpecCsvFilePath argument. The R files are written to the folder whose path
+#'is specified in the projectFolderPath arg
+#'
+#' @param webSpecCsvFilePath The path to the web spec csv file
+#' @param projectFolderPath The path to the folder where the R files will be put
+#'
+#' @return
+#' @export
+#'
+#' @examples
 buildProjectFiles <- function(webSpecCsvFilePath, projectFolderPath) {
   webSpecCsv <- read.csv(webSpecCsvFilePath)
   webSpecCsv <- data.frame(lapply(webSpecCsv, as.character), stringsAsFactors = FALSE)
   
+  # Gets all the categorical variable rows from the web spec. Categorical
+  # variable rows are ones whose VariableNameColumName ends with _cat<number>
   categoricalVariableRows <- webSpecCsv[which(grepl('_cat\\d', webSpecCsv[, VariableNameColumName])), ]
+  # Will have all the dummying code
   dummyVariableCode <- ''
   if(nrow(categoricalVariableRows) != 0) {
     for (i in 1:nrow(categoricalVariableRows)) {
+      # Split with '_' string to get an array where the first value is the name
+      # of the variable and the second value is 'cat<number>'
       variableNameSplitWithUnderscore <- strsplit(categoricalVariableRows[i, VariableNameColumName], '_')[[1]]
+      # Split with second value in variableNameSplitWithUnderscore with
+      # 'cat' to get the number of categories in thie variables
       # Cast to numeric since it has a NA value in it
       numberOfCategories <- as.numeric(strsplit(variableNameSplitWithUnderscore[[2]], 'cat')[[1]])[2]
       currentDummyVariableCode <- getCodeForDummyVariable(categoricalVariableRows[i, VariableNameColumName], numberOfCategories)
       
+      # If this is the first categorical var for whom we are generating
+      # dummying code we just append it to the entire dummying code
       if(i == 1) {
         dummyVariableCode <- glue::glue(dummyVariableCode, currentDummyVariableCode)
-      } else {
+      }
+      # Otherwise add a newlines between it so that there is space between
+      # the dummying code for each categorical var
+      else {
         dummyVariableCode <- glue::glue('{dummyVariableCode}\n\n{currentDummyVariableCode}')
       }
     }
   }
   
+  # Centering code
   centeredVariableCode <- ''
   
   if(isPortWebSpecFile(webSpecCsv) == FALSE) {
+    # Go through each web spec row
     for (i in 1:nrow(webSpecCsv)) {
+      # If the current row is a categorical variable
       if (grepl('_cat\\d', webSpecCsv[i, VariableNameColumName])) {
+        # The following 2 lines get the number of categories for this
+        # cat variable
         variableNameSplitWithUnderscore <- strsplit(webSpecCsv[i, VariableNameColumName], '_')[[1]]
         # Cast to numeric since it has a NA value in it
         numberOfCategories <- as.numeric(strsplit(variableNameSplitWithUnderscore[[2]], 'cat')[[1]])[2]
         
+        # Add centering code for each dummied var
         for (j in 1:numberOfCategories) {
+          # In RESPECT the convention we use is:
+          # 1. Centered variables have the _c in the name
+          # 2. Dummied cat vars have _<category number> in them
+          # 3. Use the above 2 facts to pass in the right values for the 
+          #    getCodeForCenteredVariable function
           centeredVariableCode <- paste(centeredVariableCode, getCodeForCenteredVariable(glue::glue('{webSpecCsv[i, VariableNameColumName]}_{j}'), glue::glue('{webSpecCsv[i, VariableNameColumName]}_{j}_c')), sep = '\n')
         }
-      } else {
+      }
+      # Otherwise it's a continuous variable
+      else {
         centeredVariableCode <- paste(centeredVariableCode, getCodeForCenteredVariable(glue::glue('{webSpecCsv[i, VariableNameColumName]}'), glue::glue('{webSpecCsv[i, VariableNameColumName]}_c')), sep = '\n')
       } 
     }
-  } else {
+  }
+  # For a PORT algorithm
+  else {
+    # Get all the variables which are centered. These are the ones with C_
+    # in the name
     centeredVariables <- webSpecCsv[which(grepl('C_', webSpecCsv[, VariableNameColumName]) & grepl('_rcs1', webSpecCsv[, VariableNameColumName]) == FALSE), ]
     # Don't center rcs variables
     centeredVariables <- centeredVariables[which(grepl('_rcs1', centeredVariables[, VariableNameColumName]) == FALSE), ]
@@ -56,17 +96,22 @@ buildProjectFiles <- function(webSpecCsvFilePath, projectFolderPath) {
   # Rcs Code
   rcsCode <- ''
   if(isPortWebSpecFile(webSpecCsv)) {
+    # All rcs variable rows
     rcsRows <- webSpecCsv[which(grepl('_rcs1', webSpecCsv[,  VariableNameColumName])), ]
     nonRcsRows <- webSpecCsv[which(grepl('_rcs1', webSpecCsv[, VariableNameColumName]) == FALSE), ]
     
     for(i in 1:nrow(rcsRows)) {
+      # Get the name of the var before the RCSing step
       variableNameWithoutRcs1 <- strsplit(rcsRows[i, VariableNameColumName], '_rcs1')[[1]][[1]]
+      # Find a variable with the name same as the variableNameWithoutRcs1 var
       variableForRcsRow <- nonRcsRows[which(grepl(variableNameWithoutRcs1, nonRcsRows[, VariableNameColumName])), ]
 
       if(nrow(variableForRcsRow) == 0) {
         stop(glue::glue('No variable for rcs {rcsRows[i, VariableNameColumnName]}'))
       }
       
+      # The RCS code is just a variable rename from the variable before
+      # RCSing step to after
       rcsCodeForRcsRow <- glue::glue('{rcsRows[i, VariableNameColumName]} <- {variableForRcsRow[1, VariableNameColumName]}')
 
       rcsCode <- paste(rcsCode, rcsCodeForRcsRow, sep='\n')
@@ -75,6 +120,7 @@ buildProjectFiles <- function(webSpecCsvFilePath, projectFolderPath) {
   
   # Interactions code
   interactionsCode <- ''
+  # Interactions rows are ones whose Interactions colums are not empty string
   interactionVariableRows <- webSpecCsv[which(webSpecCsv[, 'Interactions'] != ''), ]
   interactionsList <- buildInteractionsList(interactionVariableRows)
 
@@ -93,7 +139,18 @@ buildProjectFiles <- function(webSpecCsvFilePath, projectFolderPath) {
   cat(rcsCode, file = paste(projectFolderPath, '/rcs.R', sep = ''))
 }
 
+#' Checks whether a web spec file is for a PORT algorithm or for a RESPECT
+#' algorithm
+#'
+#' @param webSpecs The web spec file rows in a data frame
+#'
+#' @return boolean
+#' @export
+#'
+#' @examples
 isPortWebSpecFile <- function(webSpecs) {
+  # If there is a row where the variable name has a 'C_' in it then it's
+  # a PORT web spec file
   rowsWithC_ <- webSpecs[which(grepl('C_', webSpecs[, VariableNameColumName])), ]
   
   if(nrow(rowsWithC_) == 0) {
