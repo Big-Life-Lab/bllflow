e506b56f39ef7e8d22242a3a8ceda4ea480e9ebc
Doug Manuel <doug.manuel@gmail.com>
2018-11-24 23:58:28 -0500

    [feature] revise documentation, remove data.frame in function, revise label

--- R/make-event-date.R
+++ R/make-event-date.R
@@ -11,29 +11,33 @@ library(Hmisc)  # for variable labels only
 #' like to create a fictious event or censoring events.
 #' @param df (required) Data frame with event propobablity. Each row represents
 #'  an observation.
-#' @param event_probability (required) Target event probablity. Event probability.
-#' e.g. 5-year risk of death is 0.2 represents 20 \% proabality of dying over
-#' 5 years or 1825 days.
-#' @param followup_time (required) Time period for event probablity.
+#' @param event_probability (required) Target event probablity (number value
+#' from 0 to 1 representing the event probability.e.g. 5-year risk of death is 
+#' 0.2 represents 20 \% proabality of dying over 5 years or 1825 days.
+#' @param followup_time (required) Time period for event probablity (number, 
+#' usually an integer).
 #' @param label (optional) Name of event. If missing, default to 'Time to
 #' event'.
 #' @param units (optional) Time unit for event. e.g. days.
-#' #' @return List with event times. Returned list length = list length of df. Event
-#' date =  totalTime (censored), if no event.
-#' @keywords make_data 
+#' #' @return List with event times (integers) Returned list length = 
+#' list length of df. Event date =  totalTime (censored), if no event.
+#' @keywords make_data assign_first_event
 #' @examples
 #' # load the test data in the bllFlow package
 #' \dontrun{
+#' #loaod and example data.frame of observations which includes a calculation of
+#'  risk estimate (risk for death at end-of-life).
 #'  df <- as.data.frame(read.csv('inst/extdata/RESPECT-EOL_validation.csv'))
-#'  
 #'  # or 
 #'  # df <- RESPECT-EOL_validation
+#'  df_length <- nrow(df)
 #'  
 #' # each ovservation as event probablity of df$risk
-#'   df$tt_event <- make_event_date(df, df$risk, 1825, units = 'days')
+#'   df$tt_event <- make_event_date(df_length, df$risk, 1825, units = 'days')
 #'   
 #'  # add variable labeles
-#'    
+#'    df$tt_event <- make_event_date(df_length, df$risk, 1825, 
+#'    label = "end-of-life", units = 'days')
 #'   
 #' # check the label
 #'   label(df$tt_event)
@@ -46,16 +50,21 @@ library(Hmisc)  # for variable labels only
 #'   nrow(df[df$tt_event < 1825,]) / nrow(df)
 #' 
 #' # make other event data like censoring or competing events
-#'   df$ttcensor <- make_event_date(df, .01, 1825)
+#'   df$ttcensor <- make_event_date(df_length, .01, 1825)
 #' }
 #' @export make_event_date
-make_event_date <- function(df, event_probability, followup_time, label = "Time to event", 
+make_event_date <- function(num_events,
+                            event_probability, 
+                            followup_time, 
+                            label = "time to event", 
                             units = NA) {
-  # if !event_label = 'Time to event'
-  random_risk <- runif(nrow(df))  # create temporary list of random probablities
   
-  time_to_event <- ifelse(random_risk <= event_probability, as.integer(followup_time * 
-                                                                         random_risk), followup_time)
+  # if !event_label = 'Time to event'
+  random_risk <- runif(num_events)  # create temporary list of random probablities
+  
+  time_to_event <- ifelse(random_risk <= event_probability, 
+                          as.integer(followup_time * random_risk), 
+                          followup_time)
   # Don't forget the label.
   Hmisc::label(time_to_event) <- label
   units(time_to_event) <- units
