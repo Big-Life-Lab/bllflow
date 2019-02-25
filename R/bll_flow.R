a8b9ac91520fc785f52463ab6b6222d138ee65d6
yulricsequeira <yulrics@gmail.com>
2019-02-25 16:02:27 -0500

    [Feature] Added R files with specs for creating the table one long

--- R/bll_flow.R
+++ R/bll_flow.R
@@ -1,16 +1,20 @@
-#' Makes an object an instance of bll_flow and returns it. This function should
-#' be run on all the variables in the initial data set
+#' Makes the data argument an instance of 'bll_flow'
 #'
-#' @param x numeric The object to convert to bllFlow
-#' @param label string Optional label that will be attached to the object
-#'
-#' @return Returns a new bll_flow object
+#' @param data A dataframe that represents the dataset the model will be developed
+#' on
+#' @param MSW The master specification sheet for this model. An example
+#' of this worksheet is available here
+#' https://docs.google.com/spreadsheets/d/1FGy-Zg0AUMEVSqpbAoIMoS_SgD__mG2SCIU2vVwsyCI/edit?ts=5c49dc3d#gid=1367487357.
+#' CSV file should be read in using the read.csv function.
+#' @param variable_details_sheet The variable details worksheet. An example
+#' of this worksheet is available here
+#' https://docs.google.com/spreadsheets/d/1FGy-Zg0AUMEVSqpbAoIMoS_SgD__mG2SCIU2vVwsyCI/edit?ts=5c49dc3d#gid=1848193100.
+#' CSV file should be read in using the read.csv function.
+#' @return Returns the data argument now an instance of the bll_flow class with
+#' the MSW and variable_details_sheet as variables on the object.
 #' @export
 #'
 #' @examples
-bll_flow <- function(x, label="Not Defined") {
-  class(x) <- c('bll_flow', class(x))
-  attr(x, 'label') <- label
+bll_flow <- function(data, MSW, variable_details_sheet) {
   
-  invisible(x)
 }
---
+++ R/table_one_long.R
@@ -0,0 +1,13 @@
+#' Creates a table one long data frame
+#'
+#' @param bll_flow_model A bll_flow model. This is an R object that's been 
+#' returned from the bll_flow function
+#' @param table_one_long_spec The config for creating the table one long.
+#' TODO Add example here
+#' @return Returns a data frame that contains the table one long
+#' @export
+#'
+#' @examples
+bll_flow.MakeTableOneLong <- function(bll_flow_model, table_one_long_spec) {
+  
+}
