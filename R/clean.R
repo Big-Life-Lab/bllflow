1e0b47eb9431375efe9d79a15817e6f318a3e90f
yulricsequeira <yulrics@gmail.com>
2019-02-26 17:20:46 -0500

    [Feature] Adde skeletons for clean.min and clean.max methods

---
+++ R/clean.R
@@ -0,0 +1,47 @@
+#' Cleans the data using the min and outlier columns in the variables sheet of
+#' the MSW. Outlier method is applied on a row if any of the variable
+#' values for that row is less than the min value as specified in the variables
+#' sheet. Outlier checking for the column is not applied if min value is NA.
+#' 
+#' Currently supported outlier methods are:
+#' Delete - Specified as 'delete' in MSW. Deletes the row from the data. 
+#' Deleted rows are stored in the metadata variable under the deletedRows name.
+#' Missing - Specified as 'missing' in MSW. Column value for that row which does 
+#' not meet the criteria is set to NA.
+#' Not Applicable - TODO.
+#' Set to value - Specified as a number value in MSW. Column value for the row is 
+#' set to the value specified in the outlier column.
+#'
+#' @param bllFlowModel 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean.min.BLLFlow <- function(bllFlowModel) {
+  
+}
+
+#' Cleans the data using the max and outlier columns in the variables sheet of
+#' the MSW. Outlier method is applied on a row if any of the variable
+#' values for that row is greater than the max value as specified in the variables
+#' sheet. Outlier checking for the column is not applied if max value is NA.
+#' 
+#' Currently supported outlier methods are:
+#' Delete - Specified as 'delete' in MSW. Deletes the row from the data. 
+#' Deleted rows are stored in the metadata variable under the deletedRows name.
+#' Missing - Specified as 'missing' in MSW. Column value for that row which does 
+#' not meet the criteria is set to NA.
+#' Not Applicable - TODO.
+#' Set to value - Specified as a number value in MSW. Column value for the row is 
+#' set to the value specified in the outlier column.
+#'
+#' @param bllFlowModel 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean.max.BLLFlow <- function(bllFlowModel) {
+  
+}
