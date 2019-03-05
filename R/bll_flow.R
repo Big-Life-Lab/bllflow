96f41c3884582fdc32bafd5b58a7ed93eaeabf49
yulricsequeira <yulrics@gmail.com>
2019-03-05 09:35:16 -0500

    [Bug] Updated doc for BLLFLow function

--- R/bll_flow.R
+++ R/bll_flow.R
@@ -10,7 +10,6 @@
 #' of this worksheet is available here
 #' https://docs.google.com/spreadsheets/d/1QVqLKy_C185hzeQdJeOy-EeFMBXui1hZ1cB2sKqPG-4/edit#gid=1196358036.
 #' CSV file should be read in using the read.csv function.
-#' The paramater that is passed is the output of the read.csv function
 #' @return Returns the data argument with BLLFlow attached as a class name.
 #' Adds the variables and variableDetailsSheet args as variables on the
 #' data argument.
