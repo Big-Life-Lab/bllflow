b8815dca7c16933b8d7fec2dfed86d5aee8cdde4
DarkShadeKnigh <rostikisawesome@gmail.com>
2019-03-13 15:41:59 -0400

    [Feature] Created longTable for ContTable

--- R/table-one-long.R
+++ R/table-one-long.R
@@ -43,9 +43,10 @@ tables.CreateTableOneLong <- function(bllFlowModel,
     unique(unlist(tableVariablesSheet[, grepl("pbcSummaryStat", colnames(tableVariablesSheet))]))
   variablesForStrata <- variablesForStrata[variablesForStrata != ""]
   #tablesTB <-
-   # lapply(variablesForStrata , function(strataValue)
-   #   CreateCustomTableOne(strataValue, tableVariablesSheet, bllFlowModel))
-  testTable <- CreateCustomTableOne(variablesForStrata[2], tableVariablesSheet, bllFlowModel)
+  # lapply(variablesForStrata , function(strataValue)
+  #   CreateCustomTableOne(strataValue, tableVariablesSheet, bllFlowModel))
+  testTable <-
+    CreateCustomTableOne(variablesForStrata[2], tableVariablesSheet, bllFlowModel)
   #print(attributes(tablesTB[[1]]))
   longTable <-
     data.frame(
@@ -68,7 +69,8 @@ tables.CreateTableOneLong <- function(bllFlowModel,
       percentile25 = 100.100,
       percentile75 = 100.100
     )
-  AddToLongTable(testTable)
+  #print(bllFlowModel$variableDetailsSheet)
+  AddToLongTable(testTable, bllFlowModel$variableDetailsSheet)
   #longTable <- rbind(longTable, AddToLongTable(testTable))
   
   #return()
@@ -93,7 +95,7 @@ CreateCustomTableOne <-
     return(retTable)
   }
 
-AddToLongTable <- function(passedTable) {
+AddToLongTable <- function(passedTable, variableDetails) {
   # group_by_1 set to first strat
   # group by value 1 set to first strata value
   # group by label 1 set to label of the variable
@@ -104,18 +106,117 @@ AddToLongTable <- function(passedTable) {
   # variable set to variable
   strataCounter <- 1
   dimNames <- attr(passedTable$ContTable, "dimnames")
+  #print(attributes(passedTable$ContTable))
   strataAllCombinationsDataFrame <- expand.grid(dimNames)
   strataArgs <- c(strataAllCombinationsDataFrame, sep = ":")
   strataValues <- do.call(paste, strataArgs)
-  if (!is.null(passedTable$ContTable)){
-    ExtractDataFromContTable(passedTable$ContTable)
+  if (!is.null(passedTable$ContTable)) {
+    ExtractDataFromContTable(
+      passedTable$ContTable,
+      attr(passedTable$ContTable, "strataVarName"),
+      strataValues,
+      variableDetails
+    )
   }
 }
 
-ExtractDataFromContTable <- function(contTable){
-  for (i in contTable) {
-    print(nrow(contTable))
+ExtractDataFromContTable <-
+  function(contTable,
+           strataName,
+           strataValues,
+           variableDetails) {
+    longTable <-
+      data.frame(
+        groupBy1 = "DummyRow",
+        groupByValue1 = "DummyRow",
+        groupByLabel1 = "DummyRow",
+        groupByValueLabel1 = "DummyRow",
+        groupBy2 = "DummyRow",
+        groupByValue2 = "DummyRow",
+        groupByLabel2 = "DummyRow",
+        groupByValueLabel2 = "DummyRow",
+        variableCategory = 1,
+        variableCategoryLabel = "DummyRow",
+        variable = "DummyRow",
+        prevalence = 0.25,
+        n = 1000,
+        nMissing = 1000,
+        mean = 100.100,
+        sd = 100.100,
+        percentile25 = 100.100,
+        percentile75 = 100.100
+      )
+    
+    strataSplitName <-
+      unlist(strsplit(as.character(strataName), split = ":"))
+    #print(strataSplitName)
+    for (i in 1:length(contTable)) {
+      variables <- (row.names(contTable[[i]]))
+      for (row in 1:nrow(contTable[[i]])) {
+        strataSplitValues <-
+          unlist(strsplit(as.character(strataValues[[i]]), split = ":"))
+        # extract all the information for that row
+        num <- contTable[[i]][row, "n"]
+        nMiss <- contTable[[i]][row, "miss"]
+        rowMean <- contTable[[i]][row, "mean"]
+        rowSD <- contTable[[i]][row, "sd"]
+        rowPercentile25 <- contTable[[i]][row, "p25"]
+        rowPercentile75 <- contTable[[i]][row, "p75"]
+        # create the row to add to tableOne Long
+        groupByLabel1 <-
+          variableDetails[compareNA(variableDetails$variable, strataSplitName[[1]]) &
+                            compareNA(variableDetails$value, strataSplitValues[[1]]), "label"]
+        groupByValueLabel1 <-
+          variableDetails[compareNA(variableDetails$variable, strataSplitName[[1]]) &
+                            compareNA(variableDetails$value, strataSplitValues[[1]]), "value_label"]
+        groupByLabel2 <-
+          variableDetails[compareNA(variableDetails$variable, strataSplitName[[2]]) &
+                            compareNA(variableDetails$value, strataSplitValues[[2]])
+                          , "label"]
+        groupByValueLabel2 <-
+          variableDetails[compareNA(variableDetails$variable, strataSplitName[[2]]) &
+                            compareNA(variableDetails$value, strataSplitValues[[2]])
+                          , "value_label"]
+        print(
+          paste(
+            variableDetails$variable,
+            strataSplitName[[2]],
+            variableDetails$value,
+            strataSplitValues[[2]]
+          )
+        )
+        print(groupByLabel2)
+        print(groupByValueLabel2)
+        longTableRow <- data.frame(
+          groupBy1 = strataSplitName[[1]],
+          groupByValue1 = strataSplitValues[[1]],
+          groupByLabel1 = groupByLabel1,
+          groupByValueLabel1 = groupByValueLabel1,
+          groupBy2 = strataSplitName[[2]],
+          groupByValue2 = strataSplitValues[[2]],
+          groupByLabel2 = groupByLabel2,
+          groupByValueLabel2 = groupByValueLabel2,
+          variableCategory = NA,
+          variableCategoryLabel = NA,
+          variable = variables[[row]],
+          prevalence = NA,
+          n = num,
+          nMissing = nMiss,
+          mean = rowMean,
+          sd = rowSD,
+          percentile25 = rowPercentile25,
+          percentile75 = rowPercentile75
+        )
+        longTable <- rbind(longTable, longTableRow)
+      }
+    }
+    print(longTable)
   }
-  print(contTable)
-}
 
+compareNA <- function(v1,v2) {
+  # This function returns TRUE wherever elements are the same, including NA's,
+  # and false everywhere else.
+  same <- (v1 == v2)  |  (is.na(v1) & is.na(v2))
+  same[is.na(same)] <- FALSE
+  return(same)
+}
