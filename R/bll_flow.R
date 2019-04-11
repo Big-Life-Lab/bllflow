268cd5301382a689fe23e1d135cf668e6812244f
DarkShadeKnigh <rostikisawesome@gmail.com>
2019-04-11 13:46:24 -0400

    [Refactor] Added more comments and addressed some minor bugs

--- R/bll_flow.R
+++ R/bll_flow.R
@@ -32,6 +32,9 @@ library(DDIwR)
 #' # Create a bllFlow R object for the PBC model using the above variables as args
 #' library(bllFlow)
 #' pbcModel <- BLLFlow(pbc, variablesSheet, variableDetailsSheet)
+#' # Test for DDI
+#' tmp <- BLLFlow(pbc, variablesSheet, variableDetailsSheet, file.path(getwd(),
+#'  'bllFlow/extdata/test-folder/DDI/cchs-82M0013-E-2013-2014-Annual-component.xml')
 #'
 #' # Passing objects other then a dataframe will create errors
 #' #pbcModel <- BLLFlow(pbc, c(1,2,3), list(2,3,4))
@@ -41,9 +44,10 @@ BLLFlow <-
            variableDetailsSheet,
            ddiXMLMetaData = FALSE) {
     # Verify passed arg integrity for future functions
-    CheckIfDataFrame(data, "data")
-    CheckIfDataFrame(variables, "variables")
-    CheckIfDataFrame(variableDetailsSheet, "variableDetailsSheet")
+    CheckIfDataFrame(data, pkg.globals$argument.Data)
+    CheckIfDataFrame(variables, pkg.globals$argument.Variables)
+    CheckIfDataFrame(variableDetailsSheet,
+                     pkg.globals$argument.VariableDetailsSheet)
     CheckForColumnPresence(
       c(
         pkg.globals$columnNames.Min,
@@ -51,36 +55,38 @@ BLLFlow <-
         pkg.globals$columnNames.Outlier
       ),
       variables,
-      "variables"
+      pkg.globals$argument.Variables
     )
     # if the ddi metadata is supplied populate variable details sheet using it
     if (ddiXMLMetaData != FALSE) {
-      tmpOut <- list()
+      variableValueList <- list()
       labeledVariables <- list()
-      standardNAValueLabels <-
-        c("NOT STATED", "REFUSAL", "DON'T KNOW", "NOT APPLICABLE")
-      variableStartType <- character()
       catStartValue <- numeric()
       catStartLabel <- character()
       variableStartLabel <- character()
-      ddiMetaData <- DDIwR::getMetadata(ddiXMLMetaData)
+      # DDwR crates lots of cat outputs that are suppressed
+      ddiMetaData <-
+        SupressFunctionCatOutput(DDIwR::getMetadata(ddiXMLMetaData))
+      # used for parcing out additional data
       ddiObject <- xml2::as_list(xml2::read_xml(ddiXMLMetaData))
       detectedVariables <-
-        unique(variableDetailsSheet["variableStart"])
+        unique(variableDetailsSheet[pkg.globals$argument.VariableStart])
       # Find extra info about the variable low and high
       valueForHighLow <- list()
+      # Need to loop through every element because the xml2 names all variables var
       for (individualVariable in ddiObject$codeBook$dataDscr) {
         if (!is.null(attr(individualVariable, "name"))) {
           ddiElementName <- attr(individualVariable, "name")
           if (length(detectedVariables[detectedVariables$variableStart == ddiElementName, 1]) >
               0) {
             valueForHighLow[[ddiElementName]] <- individualVariable$valrng$range
-            valueForHighLow[[ddiElementName]][["TypeOfVariable"]] <- ifelse(attr(individualVariable, "intrvl")=="discrete", "cat", "cont")
+            valueForHighLow[[ddiElementName]][["Type"]] <-
+              ifelse(attr(individualVariable, "intrvl") == "discrete",
+                     "cat",
+                     "cont")
           }
         }
       }
-      populatedPreCursor <- 1
-      populatedPostCursor <- 0
       
       # Loop through every unique variable found in the VariableDetails
       for (variableToCheck in detectedVariables[, 1]) {
@@ -89,32 +95,29 @@ BLLFlow <-
           # Store the label for that variable
           variableStartLabel <-
             ddiMetaData$varlab[[variableToCheck]]
-          tmpOut <- list()
+          variableValueList <- list()
           # Check if that variable has value labels
           if (variableToCheck %in% names(ddiMetaData$vallab)) {
-            # Check if its Continues with labels for NA values
-            containsOnlyNAValues <- TRUE
             # Loop Through all the values that are stored for that variable
-            
             for (valueLabelToCheck in names(ddiMetaData$vallab[[variableToCheck]])) {
-              if (!valueLabelToCheck %in% standardNAValueLabels) {
-                containsOnlyNAValues <- FALSE
-              }
               catStartValue <-
                 ddiMetaData$vallab[[variableToCheck]][[valueLabelToCheck]]
               catStartLabel <- valueLabelToCheck
+              # Should i function out the population????
               # Needs to be a character because cant access list with decimal value or zero value
-              tmpOut[[as.character(catStartValue)]] <- list(
-                variableStartType = valueForHighLow[[variableToCheck]]$Type,
-                catStartValue = catStartValue,
-                catStartLabel = catStartLabel,
-                variableStartLabel = variableStartLabel,
-                variableStartLow = catStartValue,
-                variableStartHigh = catStartValue
-              )
+              variableValueList[[as.character(catStartValue)]] <-
+                list(
+                  variableStartType = valueForHighLow[[variableToCheck]]$Type,
+                  catStartValue = catStartValue,
+                  catStartLabel = catStartLabel,
+                  variableStartLabel = variableStartLabel,
+                  variableStartLow = catStartValue,
+                  variableStartHigh = catStartValue
+                )
             }
+            # different values for high and low as well as cat value and label
             if (valueForHighLow[[variableToCheck]]$Type == "cont") {
-              tmpOut[[as.character(variableToCheck)]] <- list(
+              variableValueList[[as.character(variableToCheck)]] <- list(
                 variableStartType = valueForHighLow[[variableToCheck]]$Type,
                 catStartValue = NA,
                 catStartLabel = NA,
@@ -123,8 +126,9 @@ BLLFlow <-
                 variableStartHigh = attr(valueForHighLow[[variableToCheck]], "max")
               )
             }
+            # in case there is no labels for the data
           } else{
-            tmpOut[[variableToCheck]] <-
+            variableValueList[[variableToCheck]] <-
               list(
                 variableStartType = valueForHighLow[[variableToCheck]]$Type,
                 catStartValue = NA,
@@ -134,13 +138,23 @@ BLLFlow <-
                 variableStartHigh = attr(valueForHighLow[[variableToCheck]], "max")
               )
           }
-          labeledVariables[[variableToCheck]] <-
-            list(Type = variableStartType, rest = tmpOut)
+          # add the list of value labels to that variable
+          labeledVariables[[variableToCheck]] <- variableValueList
         }
       }
-      finalFrame <- PopulateVariableDetails(variableDetailsSheet,
-                              labeledVariables,
-                              detectedVariables)
+      populatedVariableDeatailsSheet <-
+        PopulateVariableDetails(variableDetailsSheet,
+                                labeledVariables,
+                                detectedVariables)
+      additionalDDIMetaData <- list(
+        docDscr = ddiObject$codeBook$docDscr,
+        stdyDscr = ddiObject$codeBook$stdyDscr,
+        fileDscr = ddiObject$codeBook$fileDscr
+      )
+    } else{
+      # Set ddi related vars to null
+      additionalDDIMetaData <- NULL
+      populatedVariableDeatailsSheet <- NULL
     }
     
     bllFlowModel <-
@@ -148,148 +162,124 @@ BLLFlow <-
         data = data,
         variables = variables,
         variableDetailsSheet = variableDetailsSheet,
-        additionalDDIMetaData = list(docDscr = ddiObject$codeBook$docDscr, stdyDscr = ddiObject$codeBook$stdyDscr, fileDscr = ddiObject$codeBook$fileDscr),
-        populatedVariableDeatailsSheet = finalFrame,
-        ddiObject = ddiObject,
-        whyAreYouLikeThis = labeledVariables
+        additionalDDIMetaData = additionalDDIMetaData,
+        populatedVariableDeatailsSheet = populatedVariableDeatailsSheet
       )
     attr(bllFlowModel, "class") <- "BLLFlow"
     
     return(bllFlowModel)
   }
 
+# Used to populate the dataframe using DDI
 PopulateVariableDetails <-
   function(frameToPopulateFrom,
            labeledVariables,
            variablesBeingChanged) {
-    preCursor <- 1
+    # Used to group all the variables in the dataframe
     frameToPopulateFrom <-
       frameToPopulateFrom[order(frameToPopulateFrom$variableStart,
                                 frameToPopulateFrom$catStartValue), ]
-    frameToWriteTo <-
+    onlyDesiredVariables <-
       frameToPopulateFrom[frameToPopulateFrom$variableStart %in% names(labeledVariables),]
-    rownames(frameToWriteTo) <- NULL
-    unEditedData <-
+    variablesNotRelatedToTheDDI <-
       frameToPopulateFrom[!frameToPopulateFrom$variableStart %in% names(labeledVariables),]
-    rownames(unEditedData) <- NULL
-    variablesBeingChanged <- unique(frameToWriteTo["variableStart"])
-    finalFrame <- frameToWriteTo[0,]
+    variablesBeingChanged <-
+      unique(onlyDesiredVariables[pkg.globals$argument.VariableStart])
+    # Copy all the columns
+    finalFrame <- onlyDesiredVariables[0,]
     for (nameIndex in 1:length(names(labeledVariables))) {
       nameBeingChecked <- names(labeledVariables)[[nameIndex]]
-      postCursor <-
-        rownames(variablesBeingChanged)[which(variablesBeingChanged$variableStart == nameBeingChecked)]
+      # All the rows for the variable being checked
       rowsToCheck <-
-        frameToWriteTo[frameToWriteTo$variableStart == nameBeingChecked, ]
+        onlyDesiredVariables[onlyDesiredVariables$variableStart == nameBeingChecked, ]
       # Writes data to relavant rows and removes them from the value object
       for (rowToCheck in 1:nrow(rowsToCheck)) {
-        presentCatStartValue <- rowsToCheck[rowToCheck, "catStartValue"]
-        if (presentCatStartValue %in% names(labeledVariables[[nameBeingChecked]]$rest)) {
-          for (columnName in names(labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]])) {
-            if (columnName != "catStartValue") {
-              if (CheckIfCellIsEmpty(rowsToCheck[rowToCheck, columnName], rownames(variablesBeingChanged)[which(variablesBeingChanged$variableStart == nameBeingChecked)], columnName)) {
-                if (!labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]][[columnName]] %in% levels(rowsToCheck[,columnName])) {
-                  levels(rowsToCheck[,columnName]) <- c(levels(rowsToCheck[,columnName]),labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]][[columnName]])
-                  #print(paste(presentCatStartValue, labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]][[columnName]]))
-                  }
+        presentCatStartValue <-
+          rowsToCheck[rowToCheck, pkg.globals$argument.CatStartValue]
+        # Check if the value matches anything in the DDI object
+        if (presentCatStartValue %in% names(labeledVariables[[nameBeingChecked]])) {
+          # Populate every column with values pulled from DDI
+          for (columnName in names(labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]])) {
+            if (columnName != pkg.globals$argument.CatStartValue) {
+              # Check if there is any data precent in the cell in order to not override anything
+              if (CheckIfCellIsEmpty(rowsToCheck[rowToCheck, columnName],
+                                     rownames(rowsToCheck)[rowToCheck],
+                                     columnName,
+                                     labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]][[columnName]])) {
+                # If this has not been in the dataframe upon creation that level is added
+                if (!labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]][[columnName]] %in% levels(rowsToCheck[, columnName])) {
+                  levels(rowsToCheck[, columnName]) <-
+                    c(levels(rowsToCheck[, columnName]), labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]][[columnName]])
+                }
                 rowsToCheck[rowToCheck, columnName] <-
-                  labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]][[columnName]]
-              }else{
-                #print(levels(rowsToCheck$catStartLabel))
+                  labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]][[columnName]]
               }
             }
           }
-          labeledVariables[[nameBeingChecked]]$rest[[as.character(presentCatStartValue)]] <- NULL
-          finalFrame <- rbind(finalFrame,rowsToCheck[rowToCheck,])
-          #print(rowsToCheck[rowToCheck,])
-          # populate row with DDI data related to the value
-        }else{
-          # leave the row untouched
+          # Remove that value from the list to avoid repetition during new row creation
+          labeledVariables[[nameBeingChecked]][[as.character(presentCatStartValue)]] <-
+            NULL
+          finalFrame <- rbind(finalFrame, rowsToCheck[rowToCheck,])
+        } else{
+          # leave the row untouched if no value is matched
           finalFrame <- rbind(finalFrame, rowsToCheck[rowToCheck, ])
         }
       }
       # Create new Rows for leftover data
-      for (leftOverValue in names(labeledVariables[[nameBeingChecked]]$rest)) {
-        rowToAdd <-  frameToWriteTo[0,]
-        for (columnName in names(labeledVariables[[nameBeingChecked]]$rest[[leftOverValue]])) {
-            rowToAdd[1, columnName] <-
-              labeledVariables[[nameBeingChecked]]$rest[[leftOverValue]][[columnName]]
-            if (!labeledVariables[[nameBeingChecked]]$rest[[as.character(leftOverValue)]][[columnName]] %in% levels(rowToAdd[,columnName])) {
-              levels(rowToAdd[,columnName]) <- c(levels(rowToAdd[,columnName]),labeledVariables[[nameBeingChecked]]$rest[[as.character(leftOverValue)]][[columnName]])
-              print(paste(levels(rowToAdd[,columnName]), labeledVariables[[nameBeingChecked]]$rest[[as.character(leftOverValue)]][[columnName]]))
-            }
+      for (leftOverValue in names(labeledVariables[[nameBeingChecked]])) {
+        rowToAdd <-  onlyDesiredVariables[0,]
+        for (columnName in names(labeledVariables[[nameBeingChecked]][[leftOverValue]])) {
+          if (!labeledVariables[[nameBeingChecked]][[as.character(leftOverValue)]][[columnName]] %in% levels(rowToAdd[, columnName])) {
+            levels(rowToAdd[, columnName]) <-
+              c(levels(rowToAdd[, columnName]), labeledVariables[[nameBeingChecked]][[as.character(leftOverValue)]][[columnName]])
+          }
+          rowToAdd[1, columnName] <-
+            labeledVariables[[nameBeingChecked]][[leftOverValue]][[columnName]]
         }
-        rowToAdd[1, "variableStart"] <- nameBeingChecked
-        finalFrame <- rbind(finalFrame,rowToAdd)
+        rowToAdd[1, pkg.globals$argument.VariableStart] <-
+          nameBeingChecked
+        finalFrame <- rbind(finalFrame, rowToAdd)
       }
     }
-    finalFrame <- rbind(finalFrame, unEditedData)
-    
+    finalFrame <- rbind(finalFrame, variablesNotRelatedToTheDDI)
+    rownames(finalFrame) <- NULL
     
     return(finalFrame)
-    # for (valueBeingRecorded in labeledVariables[[nameBeingChecked]]$rest) {
-    #   rowBeingWritten <- frameToWriteTo[preCursor,]
-    #   if (rowBeingWritten[[1,"variableStart"]] != nameBeingChecked) {
-    #     rowBeingWritten <- frameToPopulateFrom[0,]
-    #     rowBeingWritten[1,"variableStartType"] <- labeledVariables[[nameBeingChecked]][["Type"]]
-    #     rowBeingWritten[1,"variableStart"] <- nameBeingChecked
-    #     rowBeingWritten[1,"catStartValue"] <- valueBeingRecorded[["catStartValue"]]
-    #     rowBeingWritten[1,"catStartLabel"] <- valueBeingRecorded[["catStartLabel"]]
-    #     rowBeingWritten[1,"variableStartLow"] <- valueBeingRecorded[["variableStartLow"]]
-    #     rowBeingWritten[1,"variableStartHigh"] <- valueBeingRecorded[["variableStartHigh"]]
-    #     rowBeingWritten[1,"variableStartLabel"] <- valueBeingRecorded[["variableStartLabel"]]
-    #   }else{
-    #     #check there is anything in value column then fill data accordingly
-    #
-    #     #check if there is data in any column if there is then create new row
-    #     rowBeingWritten <- frameToPopulateFrom[preCursor,]
-    #     if (!is.na(rowBeingWritten[1,"catStartValue"])) {
-    #       #populate using that value
-    #     }else if (!is.na(rowBeingWritten[1,"variableStartType"]) & !is.na(rowBeingWritten[1,"catStartLabel"]) & !is.na(rowBeingWritten[1,"variableStartLow"]) & !is.na(rowBeingWritten[1,"variableStartHigh"]) & !is.na(rowBeingWritten[1,"variableStartLabel"])) {
-    #       #populate as regular
-    #     }else {
-    #       # populate as if fresh row
-    #     }
-    #     rowBeingWritten[1,"variableStartType"] <- labeledVariables[[nameBeingChecked]][["Type"]]
-    #     rowBeingWritten[1,"variableStart"] <- nameBeingChecked
-    #     rowBeingWritten[1,"catStartValue"] <- valueBeingRecorded[["catStartValue"]]
-    #     rowBeingWritten[1,"catStartLabel"] <- valueBeingRecorded[["catStartLabel"]]
-    #     rowBeingWritten[1,"variableStartLow"] <- valueBeingRecorded[["variableStartLow"]]
-    #     rowBeingWritten[1,"variableStartHigh"] <- valueBeingRecorded[["variableStartHigh"]]
-    #     rowBeingWritten[1,"variableStartLabel"] <- valueBeingRecorded[["variableStartLabel"]]
-    #     preCursor <- preCursor+1
-    #     if (nameBeingChecked=="WTS_M") {
-    #       print(rowBeingWritten)
-    #     }
-    #   }
-    #   #print(rowBeingWritten)
-    #
-    # }
-    
-    #print(finalFrame)
-    # populatedPostCursor <- rownames(detectedVariables)[which(detectedVariables$variableStart == variableToCheck)]
-    # populatedVariableDetailsSheet <- append(populatedVariableDetailsSheet, variableDetailsSheet[populatedPreCursor:populatedPostCursor,])
-    # print(populatedVariableDetailsSheet)
-    # populatedPreCursor <- populatedPostCursor
-    # cat(variableToCheck,"IM IN BOIS", rownames(detectedVariables)[which(detectedVariables$variableStart == variableToCheck)],"\n")
-    
   }
 
-CheckIfCellIsEmpty <- function(cellContent, rowNumber, columnName) {
-  isEmpty <- TRUE
-  if (!is.null(cellContent) & !is.na(cellContent) & cellContent != "") {
-    print(cellContent)
-    warning(
-      paste(
-        "The cell in row ",
-        rowNumber,
-        " column ",
-        columnName,
-        " contains data that is not part of the DDI object ",
-        cellContent
+# Checks if content of cell is empty and displays appropriate warning
+CheckIfCellIsEmpty <-
+  function(cellContent,
+           rowNumber,
+           columnName,
+           ddiValue) {
+    isEmpty <- TRUE
+    if (!is.null(cellContent) &
+        !is.na(cellContent) & cellContent != "") {
+      warning(
+        paste(
+          "Row ",
+          rowNumber,
+          ":",
+          columnName,
+          " column has value \"",
+          cellContent,
+          "\" but DDI value is \"",
+          ddiValue,
+          "\". Not overwriting"
+        ),
+        call. = FALSE,
+        immediate. = TRUE
       )
-    )
-    isEmpty <- FALSE
+      isEmpty <- FALSE
+    }
+    
+    return(isEmpty)
   }
-  
-  return(isEmpty)
+
+# Prevents function from writing Cat to console
+SupressFunctionCatOutput <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
 }
--- R/variables-sheet.R
+++ R/variables-sheet.R
@@ -2,3 +2,8 @@ pkg.globals <- new.env()
 pkg.globals$columnNames.Min <- "min"
 pkg.globals$columnNames.Max <- "max"
 pkg.globals$columnNames.Outlier <- "outlier"
+pkg.globals$argument.Data <- "data"
+pkg.globals$argument.Variables <- "variables"
+pkg.globals$argument.VariableDetailsSheet <- "variableDetailsSheet" 
+pkg.globals$argument.VariableStart <- "variableStart" 
+pkg.globals$argument.CatStartValue <- "catStartValue"
