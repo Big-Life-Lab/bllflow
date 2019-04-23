718b55e4b50e4c759aa8c6882d0e5793e449ee08
Rostyslav <rostikisawesome@gmail.com>
2019-04-23 17:14:39 -0400

    [Refactor] Added comments to strings.R

--- R/strings.R
+++ R/strings.R
@@ -1,7 +1,10 @@
 pkg.globals <- new.env()
+# Variables for variables column presence check
 pkg.globals$columnNames.Min <- "min"
 pkg.globals$columnNames.Max <- "max"
 pkg.globals$columnNames.Outlier <- "outlier"
+
+# Variables for the column names in variable details
 pkg.globals$argument.Data <- "data"
 pkg.globals$argument.Variables <- "variables"
 pkg.globals$argument.VariableDetailsSheet <- "variableDetailsSheet" 
@@ -12,10 +15,14 @@ pkg.globals$argument.VariableStartLow <- "low"
 pkg.globals$argument.CatStartValue <- "value"
 pkg.globals$argument.CatStartLabel <- "valueLabelStart"
 pkg.globals$argument.VariableStartLabel <- "label"
+
+# Variables for tags in ddi file
 pkg.globals$ddiValue.Min <- "min"
 pkg.globals$ddiValue.Max <- "max"
 pkg.globals$ddiValueName.Cont <- "cont"
 pkg.globals$ddiValueName.Cat <- "cat"
+
+# Variables for actual bllFlow objects
 pkg.globals$bllFlowContent.Variables <- "variables"
 pkg.globals$bllFlowContent.VariableDetails <- "variableDetails"
 pkg.globals$bllFlowContent.PopulatedVariableDetails <- "populatedVariableDetails"
