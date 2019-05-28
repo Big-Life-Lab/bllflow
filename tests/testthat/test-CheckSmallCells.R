9440bffa400e149bfdb325c0f4961f58b677a630
DarkShadeKnigh <rostikisawesome@gmail.com>
2019-05-28 15:45:18 -0400

    [Feature] Added started test files for the rest of the functions

---
+++ tests/testthat/test-CheckSmallCells.R
@@ -0,0 +1,15 @@
+context("CheckSmallCells")
+library(bllflow)
+
+test_that("bllFlow handles passed args of wrong type", {
+  
+})
+test_that("bllFlow handles passed args with wrong/innapropriate information", {
+  
+})
+test_that("bllFlow handles empty passed args", {
+  
+})
+test_that("bllFlow display appropriate warnings when trying to overwrite rows", {
+  
+})
--- tests/testthat/test-bllFlow.R
+++ tests/testthat/test-bllFlow.R
@@ -1,5 +1,10 @@
 context("bllFlow construction")
 library(bllflow)
+library(survival)
+data(pbc)
+variablesSheet <- read.csv(system.file("extdata", "PBC-variables.csv", package = "bllflow"))
+variableDetails <- read.csv(system.file("extdata", "PBC-variableDetails.csv", package = "bllflow"))
+ddi <- ReadDDI(system.file("extdata", package = "bllflow"), "pbcDDI.xml")
 
 test_that("bllFlow can be passed no args", {
   model <- BLLFlow()
@@ -11,4 +16,35 @@ test_that("bllFlow can be passed no args", {
   expect_equal(model$populatedVariableDetails, NULL)
   expect_equal(model$ddi, NULL)
 })
+test_that("bllFlow does not populate ddi related vars when no ddi is passed", {
+  model <- BLLFlow(pbc, variablesSheet, variableDetails)
+  expect_is(model, "BLLFlow")
+  expect_is(model$data, "data.frame")
+  expect_is(model$variables, "data.frame")
+  expect_is(model$variableDetails, "data.frame")
+  expect_equal(model$additionalDDIMetaData, NULL)
+  expect_equal(model$populatedVariableDetails, NULL)
+  expect_equal(model$ddi, NULL)
+})
+test_that("bllFlow populates appropriate ddi related vars", {
+  model <- BLLFlow(pbc, variablesSheet, variableDetails,ddi)
+  expect_is(model, "BLLFlow")
+  expect_is(model$data, "data.frame")
+  expect_is(model$variables, "data.frame")
+  expect_is(model$variableDetails, "data.frame")
+  expect_is(model$additionalDDIMetaData, "list")
+  expect_is(model$populatedVariableDetails, "data.frame")
+  expect_is(model$ddi, "BLLFlowDDI")
+})
+test_that("bllFlow handles passed args of wrong type", {
 
+})
+test_that("bllFlow handles passed args with wrong/innapropriate information", {
+  
+})
+test_that("bllFlow handles empty passed args", {
+  
+})
+test_that("bllFlow display appropriate warnings when trying to overwrite rows", {
+  
+})
