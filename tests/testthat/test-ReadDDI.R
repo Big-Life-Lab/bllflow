8114e49fa3ccb5486c0ea669b42c3f5e0930a782
DarkShadeKnigh <rostikisawesome@gmail.com>
2019-06-18 14:18:32 -0400

    [Test]

--- tests/testthat/test-ReadDDI.R
+++ tests/testthat/test-ReadDDI.R
@@ -9,11 +9,11 @@ load(
 )
 
 test_that("ReadDDI creates a proper BLLFlow DDI object", {
-  #testDDI <- ReadDDI(system.file(    "extdata/testdata/ReadDDI",    "Test-1",package = "bllflow"),  "pbcDDI.xml")
-  testDDI <- ReadDDI(system.file("extdata", "testdata", package = "bllflow"), "pbcDDI.xml")
+  testDDI <- ReadDDI(system.file(    "extdata/testdata/ReadDDI",    "Test-1",package = "bllflow"),  "pbcDDI.xml")
+  #testDDI <- ReadDDI(system.file("extdata", "testdata", package = "bllflow"), "pbcDDI.xml")
   print(attributes(testDDI$variableMetaData))
-  #expect_equal(testDDI$variableMetaData[[1]], TestEnvironment$`Test-1`$standardDDI$variableMetaData$vallab)
-  fail("DEBUG")
+  expect_equal(testDDI, TestEnvironment$`Test-1`$standardDDI)
+  #fail("test")
 })
 test_that("ReadDDI throws an error if an invalid path is given", {
   #Invalid path detected Aborting
