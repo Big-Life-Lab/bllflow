f1b8826da19e4d49e98a1c22e724ce8c2b58c050
DarkShadeKnigh <rostikisawesome@gmail.com>
2019-08-19 13:05:08 -0400

    [Feature] Wrapper function skeleton

---
+++ R/step-function-recoding.R
@@ -0,0 +1,93 @@
+step_bllflowMeanimpute <-
+  function(recipe,
+           ...,
+           role = NA,
+           trained = FALSE,
+           append = TRUE,
+           suffix = "MeanImpute",
+           means = NULL,
+           trim = 0,
+           skip = FALSE,
+           id = rand_id("bllflowMeanimpute")) {
+    add_step(
+      recipe,
+      step_bllflowMeanimpute_new(
+        terms = ellipse_check(...),
+        role = role,
+        trained = trained,
+        append = append,
+        suffix = suffix,
+        means = means,
+        trim = trim,
+        skip = skip,
+        id = id
+      )
+    )
+  }
+
+step_bllflowMeanimpute_new <-
+  function(terms, role, trained, means, trim, skip, id) {
+    step(
+      subclass = "bllflowMeanimpute",
+      terms = terms,
+      role = role,
+      trained = trained,
+      append = append,
+      suffix = suffix,
+      means = means,
+      trim = trim,
+      skip = skip,
+      id = id
+    )
+  }
+
+#' @export
+prep.step_bllflowMeanimpute <- function(x, training, info = NULL, ...) {
+  col_names <- terms_select(x$terms, info = info)
+  check_type(training[, col_names])
+  
+  means <-
+    vapply(training[, col_names],
+           mean,
+           c(mean = 0),
+           trim = x$trim,
+           na.rm = TRUE)
+  step_bllflowMeanimpute_new(
+    terms = x$terms,
+    role = x$role,
+    trained = TRUE,
+    means,
+    trim = x$trim,
+    skip = x$skip,
+    id = x$id
+  )
+}
+
+#' @export
+bake.step_bllflowMeanimpute <- function(object, new_data, ...) {
+  for (i in names(object$means)) {
+    if (any(is.na(new_data[, i])))
+      new_data[is.na(new_data[, i]), i] <- object$means[i]
+  }
+  as_tibble(new_data)
+}
+
+print.step_bllflowMeanimpute <-
+  function(x, width = max(20, options()$width - 30), ...) {
+    cat("Mean Imputation for ", sep = "")
+    printer(names(x$means), x$terms, x$trained, width = width)
+    invisible(x)
+  }
+
+#' @export
+tidy.step_bllflowMeanimpute <- function(x, ...) {
+  if (is_trained(x)) {
+    res <- tibble(terms = names(x$means),
+                  model = x$means)
+  } else {
+    term_names <- sel2char(x$terms)
+    res <- tibble(terms = term_names, model = na_dbl)
+  }
+  res$id <- x$id
+  res
+}
