fbceec3f576c1cf2f16d6fbe86518e238af6924a
Rostyslav <rostikisawesome@gmail.com>
2020-06-25 13:51:34 -0400

    [Feature] Reintroduced summary_data_long

--- R/util-funcs.R
+++ R/util-funcs.R
@@ -69,30 +69,30 @@ clean_strata_values <-
 #     return(long_table)
 #   }
 #
-# # Fills group by columns with information from variable details
-# fill_in_group_by_columns <-
-#   function(strata_split_name,
-#            strata_split_values,
-#            long_table_row,
-#            variable_details) {
-#     for (group_by_index in 1:length(strata_split_name)) {
-#       long_table_row[[paste(pkg.globals$LongTable.GroupBy, group_by_index, sep = "")]] <-
-#         strata_split_name[[group_by_index]]
-#       long_table_row[[paste(pkg.globals$LongTable.GroupByValue, group_by_index, sep = "")]] <-
-#         strata_split_values[[group_by_index]]
-#
-#       if (!is.null(variable_details)) {
-#         long_table_row[[paste(pkg.globals$LongTable.GroupByLabel, group_by_index, sep = "")]] <-
-#           variable_details[is_equal(variable_details[[pkg.globals$argument.VariableStart]], strata_split_name[[group_by_index]]) &
-#                              is_equal(variable_details[[pkg.globals$argument.CatStartValue]], strata_split_values[[group_by_index]]), pkg.globals$argument.VariableStartLabel]
-#         long_table_row[[paste(pkg.globals$LongTable.GroupByValueLabel,
-#                               group_by_index,
-#                               sep = "")]] <-
-#           variable_details[is_equal(variable_details[[pkg.globals$argument.VariableStart]], strata_split_name[[group_by_index]]) &
-#                              is_equal(variable_details[[pkg.globals$argument.CatStartValue]], strata_split_values[[group_by_index]]), pkg.globals$argument.CatStartLabel]
-#
-#       }
-#     }
-#
-#     return(long_table_row)
-#   }
+# Fills group by columns with information from variable details
+fill_in_group_by_columns <-
+  function(strata_split_name,
+           strata_split_values,
+           long_table_row,
+           variable_details) {
+    for (group_by_index in 1:length(strata_split_name)) {
+      long_table_row[[paste(pkg.globals$LongTable.GroupBy, group_by_index, sep = "")]] <-
+        strata_split_name[[group_by_index]]
+      long_table_row[[paste(pkg.globals$LongTable.GroupByValue, group_by_index, sep = "")]] <-
+        strata_split_values[[group_by_index]]
+
+      if (!is.null(variable_details)) {
+        long_table_row[[paste(pkg.globals$LongTable.GroupByLabel, group_by_index, sep = "")]] <-
+          variable_details[is_equal(variable_details[[pkg.globals$argument.VariableStart]], strata_split_name[[group_by_index]]) &
+                             is_equal(variable_details[[pkg.globals$argument.CatStartValue]], strata_split_values[[group_by_index]]), pkg.globals$argument.VariableStartLabel]
+        long_table_row[[paste(pkg.globals$LongTable.GroupByValueLabel,
+                              group_by_index,
+                              sep = "")]] <-
+          variable_details[is_equal(variable_details[[pkg.globals$argument.VariableStart]], strata_split_name[[group_by_index]]) &
+                             is_equal(variable_details[[pkg.globals$argument.CatStartValue]], strata_split_values[[group_by_index]]), pkg.globals$argument.CatStartLabel]
+
+      }
+    }
+
+    return(long_table_row)
+  }
