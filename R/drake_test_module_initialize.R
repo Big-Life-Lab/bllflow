8d52253b7ef44fae1eac70007b8e3e0c1373e6c5
Rostyslav <rostikisawesome@gmail.com>
2020-11-18 14:41:41 -0500

    [Bug] Removed extra comma

--- R/drake_test_module_initialize.R
+++ R/drake_test_module_initialize.R
@@ -1,7 +1,7 @@
 harmonize_CCHS_2001 <- function(data = "cchs2001_p", variables=c("predictor"), database_name = "cchs2001_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -10,7 +10,7 @@ harmonize_CCHS_2001 <- function(data = "cchs2001_p", variables=c("predictor"), d
 harmonize_CCHS_2003 <- function(data = "cchs2003_p", variables=c("all"), database_name = "cchs2003_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -19,7 +19,7 @@ harmonize_CCHS_2003 <- function(data = "cchs2003_p", variables=c("all"), databas
 harmonize_CCHS_2005 <- function(data = "cchs2005_p", variables=c("all"), database_name = "cchs2005_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -28,7 +28,7 @@ harmonize_CCHS_2005 <- function(data = "cchs2005_p", variables=c("all"), databas
 harmonize_CCHS_2007_2008 <- function(data = "cchs2007_2008_p", variables=c("all"), database_name = "cchs2007_2008_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -37,7 +37,7 @@ harmonize_CCHS_2007_2008 <- function(data = "cchs2007_2008_p", variables=c("all"
 harmonize_CCHS_2010 <- function(data = "cchs2010_p", variables=c("all"), database_name = "cchs2010_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -46,7 +46,7 @@ harmonize_CCHS_2010 <- function(data = "cchs2010_p", variables=c("all"), databas
 harmonize_CCHS_2012 <- function(data = "cchs2012_p", variables=c("all"), database_name = "cchs2012_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
@@ -55,7 +55,7 @@ harmonize_CCHS_2012 <- function(data = "cchs2012_p", variables=c("all"), databas
 harmonize_CCHS_2014 <- function(data = "cchs2014_p", variables=c("all"), database_name = "cchs2014_p", variable_details = "variable_details", custom_function_path = "R/rec_custom-functions.R"){
   #Since role and data was detected the config could be used to load whats needed for this generation
   config <- config::get()
-  data <- config$data[[data]],
+  data <- config$data[[data]]
   variables <- bllflow:::select_vars_by_role(variables,config$variables)
   
   # This is where the analyst would fill in the rest????
